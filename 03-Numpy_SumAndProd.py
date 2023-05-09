484ce50bcdc45384c94ef3ec2cfd6a5e5bf8d481
Eray <77747901+Eray-OZ@users.noreply.github.com>
2023-05-09 22:24:26 +0300

    Create 03-Numpy_SumAndProd.py

---
+++ 03-Numpy_SumAndProd.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+dim = input().strip().split(' ')
+arr = []
+
+for _ in range(int(dim[0])):
+    I = input().strip().split(' ')
+    I = list(map(int, I))
+    arr.append(I)
+
+sum = np.sum(arr, axis=0)
+
+result = np.prod(sum)
+
+print(result)
+    
+
+
+
